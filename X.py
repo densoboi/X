f88b613585f4afc9a1f41e389f16272ce0960bfe
densoboi <denso.boi@gmail.com>
2025-11-28 00:04:18 +1000

    Update X.py

--- X.py
+++ X.py
@@ -3,8 +3,8 @@ import requests
 import time
 
 # X API setup
-client = tweepy.Client(bearer_token='YOUR_BEARER_TOKEN')
-discord_webhook = 'YOUR_DISCORD_WEBHOOK_URL'
+client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAAOio5gEAAAAAEzwCsA%2B74hk81TVueBsSNW%2Bwa%2Fo%3D7GyzbyCZHwJHSpwu90eqQjkoyRdqDLBSATAHVCpnEJw35AzXp4')
+discord_webhook = 'https://discord.com/api/webhooks/1443599784324890655/ps20oZhs-k7O5Qs7g5HLpaArSokeSzE2ftVZS4rLi8CKF2thpeGrH1_tgZUe_lR2vIQ6'
 user_id = client.get_user(username='exampleuser').data.id
 
 last_tweet_id = None
